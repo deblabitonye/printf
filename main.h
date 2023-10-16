ceda18e439aee5b495611bc5b0f1d43d2b8bc603
deblabitonye <bitonyedebra@gmail.com>
2023-10-16 06:50:04 -0700

    main.h

--- main.h
+++ main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _printf(const char *format, ...);
 int _putchar(char c);
 int printf_char(va_list val);
 int Print_string(va_list val);
 int _strlen(char *str);
 int _strlenc(const char *str);
+
 
 #endif

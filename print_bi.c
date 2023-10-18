ec4337ebcff7b5fa06e29f429b906dd48fe916c9
Ismael045 <adamismael297@gmail.com>
2023-10-18 00:42:53 -0700

    changes

--- print_bi.c
+++ print_bi.c
@@ -10,12 +10,11 @@ int print_bin(va_list val)
 	int flag = 0;
 	int count = 0;
 	int i, a = 1, b;
-	unsigned int num = va_arg(val,unsigned int);
-	unsigned int k;
+	unsigned int k = va_arg(val,unsigned int);
 
 	for (i = 0; i< 32; i++)
 	{
-		k = ((a << (32 - i)) & num);
+		k = ((a << (32 - i)));
 		if (k >> (31 - i))
 			flag = 1;
 		if (flag)
